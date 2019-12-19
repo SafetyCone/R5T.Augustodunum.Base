299119f5150b945f30642e4581051994437839e0
MinexAutomation <minex.automation@gmail.com>
2019-12-18 23:38:09 -0800

    Added repository URL-to-local relative directory path mappings provider service definition interface type.

---
+++ source/R5T.Augustodunum.Base/Code/Services/Definitions/IRepositoryUrlToLocalRelativeDirectoryPathMappingsProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+using R5T.Augustodunum.Types;
+
+
+namespace R5T.Augustodunum
+{
+    public interface IRepositoryUrlToLocalRelativeDirectoryPathMappingsProvider
+    {
+        IEnumerable<RepositoryUrlToLocalRelativeDirectoryPathMapping> GetRepositoryUrlToLocalRelativeDirectoryPathMappings();
+    }
+}
