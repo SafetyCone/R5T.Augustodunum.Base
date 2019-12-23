424c9948a98edafd923c7f772d1d24395173956b
MinexAutomation <minex.automation@gmail.com>
2019-12-22 23:37:05 -0800

    Added service definition for adjusting repository URLs for the mapping file.

---
+++ source/R5T.Augustodunum.Base/Code/Services/Definitions/IMappingRemoteRepositoryUrlAdjuster.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace R5T.Augustodunum
+{
+    /// <summary>
+    /// Adjusts remote repository URLs to conform to a specified standard.
+    /// Useful for standardizing SVN repository URLs that end with ".git/trunk" and Git repository URLs that just end with ".git".
+    /// </summary>
+    public interface IMappingRemoteRepositoryUrlAdjuster
+    {
+        string AdjustRemoteRepositoryUrl(string repositoryUrl);
+    }
+}
